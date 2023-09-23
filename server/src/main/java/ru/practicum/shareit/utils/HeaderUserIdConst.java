57b51e4d40fe65f5757963ab1e89dcc692ab41d5
mshamiNN52 <124524683+mshamiNN52@users.noreply.github.com>
2023-09-23 20:15:33 +0300

    Delete server directory

--- server/src/main/java/ru/practicum/shareit/utils/HeaderUserIdConst.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.utils;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class HeaderUserIdConst {
-    public static final String HEADER_USER_ID = "X-Sharer-User-Id";
-}
